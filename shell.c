903f0e41f01d52b70ab0cd4d27a81605673abc75
turturicarv2 <daniel.plamadeala03@gmail.com>
2024-10-02 14:55:24 +0300

    add change directory command

--- shell.c
+++ shell.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define TRUE 1
 #define FALSE 0
+#define SUCCESS 0
+#define MAX_STRING_LENGTH 100
 
 char **parse_line(char *command) {
     char *token = strtok(command, " ");
@@ -52,11 +55,11 @@ char *get_user_input(void) {
         exit(1);
     }
 
-    /* Get the command input from the user */
+    /* get the command input from the user */
     printf("Type something: ");
     characters = getline(&buffer, &bufsize, stdin);
 
-    /* Remove the newline character */
+    /* remove the newline character */
     if (buffer[characters - 1] == '\n') {
         buffer[characters - 1] = '\0';
     }
@@ -64,18 +67,63 @@ char *get_user_input(void) {
     return buffer;
 }
 
+void print_current_directory() {
+    char string[MAX_STRING_LENGTH];
+    printf("%s\n", getcwd(string, MAX_STRING_LENGTH));
+}
+
+void change_directory(char **command_arguments) {
+    __uint8_t argument_length = MAX_STRING_LENGTH + strlen(command_arguments[1]);
+    char string[MAX_STRING_LENGTH];
+    char *relative_path = (char *) malloc(argument_length * sizeof(char));
+
+    /* creating the relative path */
+    strcpy(relative_path, getcwd(string, MAX_STRING_LENGTH));
+    strcat(relative_path, "/");
+    strcat(relative_path, command_arguments[1]);
+
+    if(chdir(command_arguments[1]) == SUCCESS) { 
+        /* case for absolute path */
+        printf("New working directory: ");
+        print_current_directory();
+    } else if(chdir(relative_path) == SUCCESS) { 
+        /* case for relative path */
+        printf("New working directory: ");
+        print_current_directory();
+    } else {
+        /* case for unrecognized path */
+        printf("%s: No such file or directory\n", command_arguments[1]);
+    }
+
+    free(relative_path);
+}
+
 void shell_loop(void) {
     while(TRUE) {
         char *command = get_user_input();
+        char **command_arguments = parse_line(command);
 
-        if(strcmp(command, "exit") == 0) {
+        /* command checking */
+        if(strcmp(command_arguments[0], "exit") == SUCCESS) {
             return;
+        } else if(strcmp(command_arguments[0], "cd") == SUCCESS) {
+            change_directory(command_arguments);
+        } else if(strcmp(command_arguments[0], "pwd") == SUCCESS) {
+            print_current_directory();
+        } else {
+            printf("Command %s not found\n", command_arguments[0]);
         }
 
-        char **command_arguments = parse_line(command);
-        
-        //do what you have to do with command arguments
+        /* memory deallocation */
+        __uint8_t number_of_arguments = sizeof(command_arguments)/sizeof(char *);
+        __uint8_t argument_index;
 
+        /* firstly deallocate the strings */
+        for(argument_index = 0; argument_index < number_of_arguments; ++argument_index) {
+            free(command_arguments[argument_index]);
+        }
+
+        /* second, deallocate the pointers */
         free(command_arguments);
         free(command);
     }
